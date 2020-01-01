fd60c99f7750d2569968741ceef264d26a65166d
vagrant <vagrant@homestead>
2020-01-01 03:18:48 +0000

    first laravel deployment to heroku

--- resources/views/application.blade.php
+++ resources/views/application.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="api-base-url" content="{{ url('/') }}"/>
-    <title>@lang('panel.title')</title>
+    <title>Pizza ordering platform</title>
 </head>
 <body>
 <p>Backend Project of an online pizza ordering platform.</p>
